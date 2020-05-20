f8f23b0b92a94a8e6277fa7b43dc392096f1790c
YnPagani <yuripagani@gmail.com>
2020-05-20 12:11:14 -0300

    Initial commit

---
+++ section19/challenge_4/src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+int main(){
+
+    std::ifstream in_file{"romeoandjuliet.txt"};
+    std::ofstream out_file{"romeoandjuliet_line.txt"};
+
+    if(!in_file.is_open() || !out_file.is_open()){
+        std::cerr << "Problem with streams" << std::endl;
+        return 1;
+    }
+
+    std::string line{};
+    int line_number{1};
+
+    while(!in_file.eof()){
+        std::getline(in_file, line);
+
+        out_file << std::setw(10) << std::left << line_number << line << std::endl;
+        line_number++;
+    }
+    std::cout << "Process done!" << std::endl;
+    
+    in_file.close();
+    out_file.close();
+
+    return 0;
+}
